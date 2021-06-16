194ad44c0e51e5c414645d8d3a4b149bd399fdd0
Luthianni Alves <57681298+Luthianni@users.noreply.github.com>
2021-06-15 23:48:38 -0300

    Configuração do databind  two-way .

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,9 +14,10 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule
      ],
-  exports: [HomeComponent]
+  exports: [HomeComponent],
   
 })
 export class HomeModule { }
